d0238bb36fa7b34d7b33c05a5a46c1af811a6c5c
Magadien Slamdien <mag.slamdien@gmail.com>
2018-11-03 18:49:29 +0200

    Chat menu

--- app/src/main/java/com/hybridsoftsolutions/letschat/activities/ChatActivity.java
+++ app/src/main/java/com/hybridsoftsolutions/letschat/activities/ChatActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.hybridsoftsolutions.letschat.R;
 import com.hybridsoftsolutions.letschat.models.Chat;
@@ -23,11 +24,45 @@ public class ChatActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.chat_menu, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
+
+        switch (item.getItemId())
+        {
+            case R.id.home:
+                finish();
+                break;
+            case R.id.action_chat_video:
+                new Toast(this).makeText(this, "Video chat", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_chat_call:
+                new Toast(this).makeText(this, "Call chat", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_chat_view_contact:
+                new Toast(this).makeText(this, "View contact", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_chat_media:
+                new Toast(this).makeText(this, "View contact", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_chat_search:
+                new Toast(this).makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_chat_mute_notifications:
+                new Toast(this).makeText(this, "Mute notifications", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_chat_wallpaper:
+                new Toast(this).makeText(this, "Wallpaper", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_chat_more:
+                new Toast(this).makeText(this, "More", Toast.LENGTH_SHORT).show();
+                break;
         }
-        return false;
+
+        return true;
     }
 }
